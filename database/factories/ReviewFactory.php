0fbea33e2e9318f93d1494ad8191e628da5f3877
Sypth <105406568+Sypth@users.noreply.github.com>
2024-07-19 14:47:26 +0800

    Creating Laravel Project File and setting up the models and migration

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReviewFactory extends Factory
+{
+    protected $model = Review::class;
+
+    public function definition(): array
+    {
+        return [
+            'book_id' => null,
+            'review' => $this->faker->paragraph,
+            'rating' => $this->faker->numberBetween(1, 5),
+            'created_at' => $this->faker->dateTimeBetween('-2 years'),
+            'updated_at' => $this->faker->dateTimeBetween('created_at', 'now'),
+        ];
+    }
+
+    public function good()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => $this->faker->numberBetween(4, 5),
+            ];
+        });
+    }
+
+    public function average()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => $this->faker->numberBetween(2, 3),
+            ];
+        });
+    }
+
+    public function bad()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => $this->faker->numberBetween(1, 3),
+            ];
+        });
+    }
+}
